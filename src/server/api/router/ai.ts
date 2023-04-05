42a03e7dc23964ff44bbe10715d6b9e49eab193e
Shayant Sital <shayant@tuta.io>
2023-04-05 01:16:12 -0300

    chore: :wastebasket: removed logs

--- src/server/api/router/ai.ts
+++ src/server/api/router/ai.ts
@@ -4,11 +4,7 @@ import { TRPCError } from "@trpc/server";
 
 export const aiRouter = createTRPCRouter({
   sqlTranslator: aiProcedure.input(z.object({ query: z.string() })).query(async ({ input, ctx }) => {
-    console.log(123);
-
     try {
-      console.log("hello");
-
       const response = await ctx.openai.createCompletion({
         model: "text-davinci-003",
         prompt: `Translate this natural language into SQL: \n\n "${input.query}" \n\n SQL Query:`,
@@ -19,8 +15,6 @@ export const aiRouter = createTRPCRouter({
         presence_penalty: 0.5,
         logprobs: 10,
       });
-
-      console.log(response.data);
 
       return {
         data: response.data.choices[0]!.text,
