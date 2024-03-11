45902d48712bbf42602bb5b8421936366a3d9a69
Shayant Sital <shayant@tuta.io>
2024-03-11 04:15:57 -0300

    Delete unused route.ts file

--- src/app/gen/qr/[val]/route.ts
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-import { type NextRequest } from "next/server";
-
-export const GET = ({ params: { val } }: { params: { val: string } }) => {
-  redirect(`/qrcode-generator?val=${val}`);
-};
